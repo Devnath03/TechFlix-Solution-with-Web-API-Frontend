8d63b4a138907258fadcd85de5852e6e9f33bfc9
Devnath03 <jmdevnath.c@gmail.com>
2025-02-22 01:28:07 +0530

    Add Routes

--- techfix_frontend/components/RouterPage.js
+++ techfix_frontend/components/RouterPage.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Login from './Login'; 
+import Registration from './Registration'; 
+import Dashboard from './Dashboard';
+import Orders from './Orders';
+import Profile from './Profile';
+import Cart from './Cart';
+
 
 export default function RouterPage (){
     return(
-        <div>Router</div>
+        <Router>
+            <Routes>
+                <Router path='/' element={<Login/>}/>
+                <Router path='/' element={<Registration/>}/>
+                <Router path='/' element={<Dashboard/>}/>
+                <Router path='/' element={<Orders/>}/>
+                <Router path='/' element={<Profile/>}/>
+                <Router path='/' element={<Cart/>}/>
+            </Routes> 
+        </Router>
+
+
     )
 }
